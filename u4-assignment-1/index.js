074e6d854ac2fe8bf65014e12e4b60cdd98a9524
khushi Gupta <95909861+khushi89012@users.noreply.github.com>
2022-03-03 16:26:48 +0530

    assignment-1

---
+++ u4-assignment-1/index.js
@@ -0,0 +1,10 @@
+const add = require("./add.js");
+const divide = require("./divide");
+const multiply = require("./multiply");
+const subtract = require("./subtract");
+
+console.log(add(5,5))
+console.log(divide(5,5))
+console.log(multiply(5,5))
+console.log(subtract(5,5))
+   
